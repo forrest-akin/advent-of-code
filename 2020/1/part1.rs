453ecd3a120a9ba23b972b926b391eba71ca0952
fakin <fakin@cj.com>
2020-12-08 07:48:46 -0800

    part 1 refactoring

--- 2020/1/part1.rs
+++ 2020/1/part1.rs
@@ -1,28 +1,29 @@
 use std::collections::HashMap;
 use std::fs;
 
-
 fn main() {
-    let entries = parse_input(read_input("input"));
-    let (x, y) = find_two_sum(2020, &entries);
+    let raw_input = read_input("input").unwrap();
+    let numbers = parse_input(&raw_input).unwrap();
+    let (x, y) = find_two_sum(2020, &numbers).expect("No pair found!");
     println!("{}", x * y);
 }
 
-fn read_input(file_path: &str) -> String {
-    fs::read_to_string(file_path).expect("unable to read input!")
-}
-
-fn parse_input(input: String) -> Vec<i32> {
-    input.lines().map(str::parse).collect::<Result<_, _>>().unwrap()
-}
-
-fn find_two_sum(target: i32, numbers: &Vec<i32>) -> (i32, i32) {
+fn find_two_sum(target: i32, numbers: &[i32]) -> Option<(i32, i32)> {
     let diff_map = key_by_diff(target, &numbers);
-    let y = numbers.iter().find_map(|x| diff_map.get(&x)).unwrap();
-    let x = diff_map.get(y).unwrap();
-    (*x, *y)
+    numbers.iter()
+    .find_map(|&x| diff_map.get(&x).map(|&y| (x, y)))
 }
 
-fn key_by_diff(target: i32, numbers: &Vec<i32>) -> HashMap<i32, i32> {
+fn key_by_diff(target: i32, numbers: &[i32]) -> HashMap<i32, i32> {
     numbers.iter().map(|&x| (target - x, x)).collect()
+}
+
+fn parse_input(input: &str) -> Result<Vec<i32>, &str> {
+    input.lines()
+    .map(|line| line.parse().map_err(|_| "ParseError: each row must be an integer"))
+    .collect()
+}
+
+fn read_input(file_path: &str) -> Result<String, &str> {
+    fs::read_to_string(file_path).map_err(|_| "IOError: unable to read input")
 }
--- 2020/1/part2.rs
+++ 2020/1/part2.rs
@@ -1,31 +1,32 @@
 use std::collections::HashMap;
 use std::fs;
-use std::io::Error;
-use std::num::ParseIntError;
 
 
 fn main() {
-    let raw_input = read_input( "input" ).expect( "unable to read input!" );
-    let input = parse_input( &raw_input ).expect( "failed to parse input!" );
-    let (x, y, z) = find_three_sum( 2020, &input ).expect( "no matching triple found for input!" );
+    let raw_input = read_input("input").unwrap();
+    let input = parse_input(&raw_input).unwrap();
+    let (x, y, z) = find_three_sum(2020, &input).expect("No triple found!");
     println!("{}", x * y * z)
 }
 
-fn read_input( file_path: &str ) -> Result<String, Error> {
-    fs::read_to_string( file_path )
-}
-
-fn parse_input( input: &str ) -> Result<Vec<i32>, ParseIntError> {
-    input.lines().map(str::parse).collect()
-}
-
-fn find_three_sum( target: i32, numbers: &[i32] ) -> Option<(i32, i32, i32)> {
+fn find_three_sum(target: i32, numbers: &[i32]) -> Option<(i32, i32, i32)> {
     let diff_map = key_by_diff( target, &numbers );
-    numbers.iter().find_map( |&x|
-        numbers.iter().find_map( |&y|
-            diff_map.get( &(x + y) ).map( |&z| (x, y, z) ) ) )
+    numbers.iter()
+    .find_map(|&x|
+        numbers.iter()
+        .find_map(|&y| diff_map.get(&(x + y)).map(|&z| (x, y, z))))
 }
 
-fn key_by_diff( target: i32, numbers: &[i32] ) -> HashMap<i32, i32> {
-    numbers.iter().map( |&x| ( target - x, x ) ).collect()
+fn key_by_diff(target: i32, numbers: &[i32]) -> HashMap<i32, i32> {
+    numbers.iter().map( |&x| (target - x, x) ).collect()
+}
+
+fn parse_input(input: &str) -> Result<Vec<i32>, &str> {
+    input.lines()
+    .map(|line| line.parse().map_err(|_| "ParseError: each row must be an integer"))
+    .collect()
+}
+
+fn read_input(file_path: &str) -> Result<String, &str> {
+    fs::read_to_string(file_path).map_err(|_| "IOError: unable to read input")
 }
