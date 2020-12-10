f8d0736154c2ce534885422d31869d2ac3c94371
fakin <fakin@cj.com>
2020-12-09 20:22:06 -0800

    use `product()` and `next()`

--- 2020/3/part2.rs
+++ 2020/3/part2.rs
@@ -4,15 +4,15 @@ use std::fs;
 fn main() {
     let raw_input = fs::read_to_string("input").expect("IOError: unable to read input");
     let slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
-    let product =
+    let product: usize =
         slopes.iter()
         .map(|slope| count_trees(*slope, &raw_input))
-        .fold(1, |x, y| x * y);
+        .product();
     println!("{}", product)
 }
 
 fn count_trees((x, y): (usize, usize), input: &str) -> usize {
-    let row_len = input.lines().nth(0).unwrap_or(&"").len();
+    let row_len = input.lines().next().unwrap_or(&"").len();
     let mut indexer = (0..row_len).cycle();
     indexer.next(); // first step is from 0, not to 0
 
