c219a24e4e9639a51b7ab8664ff287cc9de5dd11
fakin <fakin@cj.com>
2021-01-06 14:52:27 -0800

    some refactoring

--- 2020/9/part2.rs
+++ 2020/9/part2.rs
@@ -5,27 +5,26 @@ use std::fs;
 fn main() {
     let raw_input = fs::read_to_string("input").expect("IOError: unable to read input");
     let numbers = parse_input(&raw_input).unwrap();
-    let outlier = find_outlier(&numbers).unwrap();
-    let code = crack_the_code(outlier, &numbers);
-    println!("{}", code);
+    let outlier = find_outlier(&numbers).expect("No outlier found");
+    let encryption_weakness = find_encryption_weakness(outlier, &numbers).expect("No matching subarray found");
+    println!("{}", encryption_weakness);
 }
 
-fn crack_the_code(outlier: i64, numbers: &[i64]) -> i64 {
+fn find_encryption_weakness(outlier: i64, numbers: &[i64]) -> Option<i64> {
+    let sub_array = find_sub_array_with_sum(outlier, numbers)?;
+    Some(*sub_array.iter().min()? + *sub_array.iter().max()?)
+}
+
+fn find_sub_array_with_sum(target: i64, numbers: &[i64]) -> Option<Vec<&i64>> {
     let mut sum: i64 = 0;
-    let outlier_sum_sub_array = numbers.iter().fold(Vec::new(), |mut sub_array, x| {
-        while outlier < sum { sum -= sub_array.remove(0); }
-        if sum == outlier { return sub_array; }
+    let sub_array = numbers.iter().fold(Vec::new(), |mut sub_array, x| {
+        while target < sum { sum -= sub_array.remove(0); }
+        if sum == target { return sub_array; }
         sum += x;
         sub_array.push(x);
         sub_array
     });
-    match (
-        outlier_sum_sub_array.iter().min(),
-        outlier_sum_sub_array.iter().max()
-    ) {
-        (Some(min), Some(max)) => *min + *max,
-        _ => -1,
-    }
+    Some(sub_array).filter(|_| sum == target)
 }
 
 fn find_outlier(numbers: &[i64]) -> Option<i64> {
